2540a7b32530d962f23be8c579dae52814761033
ivan ceras <ivanceras@gmail.com>
2015-07-28 00:46:58 +0800

    Commiting docs to github pages

---
+++ codegenta/generator/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["generate_all","this is the default config generation"],["generate_tables","if only_tables is empty vec, then generate_all"],["get_all_tables",""],["get_schemas","get the database schema"],["get_tables_in_schema","get all tables with schema name"]],"struct":[["Config","configuration for generating code"]]});
