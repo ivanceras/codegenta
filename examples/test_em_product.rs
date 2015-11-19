dd30621064b52d7c1ab8ef901a51d633952f1d1b
ivanceras <ivanceras@gmail.com>
2015-11-19 15:57:09 +0800

    add examples for using em (EntityManager)

---
+++ examples/test_em_product.rs
@@ -0,0 +1,23 @@
+extern crate rustorm;
+extern crate uuid;
+extern crate chrono;
+extern crate rustc_serialize;
+
+
+use rustorm::pool::ManagedPool;
+
+use rustorm::em::EntityManager;
+use gen::bazaar::Product;
+
+
+mod gen;
+
+/// run using  cargo run --release --example test_em_product
+fn main(){
+    let pool = ManagedPool::init("postgres://postgres:p0stgr3s@localhost/bazaar_v6",1).unwrap();
+    let db = pool.connect().unwrap();
+    
+    let em = EntityManager::new(db.as_ref());
+    let products  = em.get_all::<Product>();
+    println!("products: {:#?}", products);
+}
