cc990d5e3e61e4b3edc1f3524522ca5b644c2229
ivanceras <ivanceras@gmail.com>
2015-11-24 17:04:42 +0800

    using rustorm 0.3.1

--- examples/test_em_insert_product.rs
+++ examples/test_em_insert_product.rs
@@ -17,7 +17,7 @@ mod gen;
 
 /// run using  cargo run --release --example test_em_insert_product
 fn main(){
-let pool = ManagedPool::init("postgres://postgres:p0stgr3s@localhost/bazaar_v6",1).unwrap();
+let pool = ManagedPool::init("postgres://postgres:p0stgr3s@localhost/bazaar_v7",1).unwrap();
     let db = pool.connect().unwrap();
     
     let em = EntityManager::new(db.as_ref());
@@ -30,7 +30,7 @@ let pool = ManagedPool::init("postgres://postgres:p0stgr3s@localhost/bazaar_v6",
             created_by: Some(Uuid::new_v4()),
             updated: UTC::now(),
             updated_by: Some(Uuid::new_v4()),
-            priority: None,
+            priority: Some(1.0),
             help: Some("A transparent chair".to_owned()),
             active: true,
             product_id: Uuid::new_v4(),
@@ -39,11 +39,11 @@ let pool = ManagedPool::init("postgres://postgres:p0stgr3s@localhost/bazaar_v6",
             price: Some(100.00),
             use_parent_price: Some(false),
             unit: Some("each".to_owned()),
-            tags: None, //Some(Json::String("glass".to_owned())),
-            info: None, //Some(Json::String("transparent glass chair".to_owned())),
-            seq_no: None,
-            upfront_fee: None,
-            barcode: None,
+            tags: Some(Json::I64(1)), //Some(Json::String("glass".to_owned())),
+            info: Some(Json::I64(1)),//Some(Json::String("transparent glass chair".to_owned())),
+            seq_no: Some(10),
+            upfront_fee: Some(0.00),
+            barcode: Some("10101010".to_owned()),
             owner_id: Some(Uuid::new_v4()),
             currency_id: Some(Uuid::new_v4()),
             owner: None,
